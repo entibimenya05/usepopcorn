32157438053bd4c00d93aa2003ebae58c45d0d1f
emmanuel <entibimenya03@gmail.com>
2025-03-12 14:03:56 +0000

    buliding a reusable star rating component

---
+++ src/StarRating.js
@@ -0,0 +1,29 @@
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
+const starContainerStyle = {
+  display: "flex",
+  gap: "4px",
+};
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
+};
+//allow the user to set maximum rating so accept the prop from App.js here:maxRating
+//pass it into length instead of hard coding it by using a number
+//set a default value of  say 5:{maxRating=5}
+export default function StarRating({ maxRating = 5 }) {
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {/*generate the stars dynamically;i+1 because i is zero based*/}
+        {Array.from({ length: maxRating }, (_, i) => (
+          <span>S{i + 1}</span>
+        ))}
+      </div>
+      <p style={textStyle}>10</p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import StarRating from "./StarRating";
+//import "./index.css";
+//import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    {/*allow the user to set the maxRating*/}
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
+    <StarRating />
   </React.StrictMode>
 );
